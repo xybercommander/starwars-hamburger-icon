5b995ecc013d3e272f0de98e832b8b193f5e7928
Samrat Mukherjee <samratmukherjee.sps@gmail.com>
2020-12-06 20:08:18 +0530

    removed the card animation

--- main.js
+++ main.js
@@ -9,14 +9,3 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
-
-// const container = document.querySelector(".menu-bg");
-
-// // Move animation
-// container.addEventListener('mousemove', (e) => {
-//     // console.log(e.pageX, e.pageY);
-//     let xAxis = (window.innerWidth / 2 - e.pageX) / 7;
-//     let yAxis = (window.innerHeight / 2 - e.pageY) / 7;
-
-//     container.style.transform = `rotateY(${yAxis}deg) rotateX(${xAxis}deg)`;
-// })
